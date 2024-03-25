2e76a11b80b8c666be65b25f075e6de37bb3079b
Riddesh Sonawane <sonawaneriddesh@gmail.com>
2024-03-25 20:58:01 +0000

    first change

--- src/Body.jsx
+++ src/Body.jsx
@@ -12,6 +12,19 @@ export default function BodyCompponents (){
                 <div className='card-content'>
                         <div className='card-front'>
                             Assistant Systems Engineer
+                            <p>Gain Comprehensive knowledge of the React based application.
+•Developing, maintaining and fixing existing code.
+•Front-end focused support role. Tweaking and enhancing application features.
+•Working with React, JavaScript, HTML, CSS and Java for developmentstack.
+•Triaging and investigating raised issues and resolve them escalating issuesif found to be bugs.
+•Working on optimizing database queries.
+•Continuously analyzing and fixing bugsfor better end-user experience.
+•Make sure the SLAs are met working in a time constraint atmosphere and adherence to KPIsto meet client
+demands.
+•Developed excellent business communication skills.
+•Extensively worked with both Windows and MacOS systems.
+•Maintaining high standard coding and following best practices.
+</p>
                         </div>
                         <div className='card-back'>
                             Product Support Analyst
